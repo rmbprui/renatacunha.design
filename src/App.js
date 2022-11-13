ad0811599100f52f4d9bc8ebbe0c00c09e4f76f1
rmbprui <rmbp.rui@gmail.com>
2022-11-13 23:15:21 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="bg-neutral-900 min-h-screen text-white">
+      <header className="text-center">
+        <h1 className="text-5xl">Hello World!</h1>
+        <button className="border-none rounded bg-primary px-5 py-2">
+          Primary
+        </button>
       </header>
     </div>
   );
